7e31051d21506c6c8eb9f61c09f36c2f91598324
Kim_SeulBee <stella_62@naver.com>
2025-07-25 17:41:35 +0900

    fix: errormiddlewaare 환경별 로그제어 추가

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -8,13 +8,14 @@ export default function errorHandler(err: CustomError, req: Request, res: Respon
   const statusCode = err.status || 500;
   const message = err.message || '서버 내부 오류';
 
-  // 에러 로깅
-  // eslint-disable-next-line no-console
-  console.error(`[ERROR] ${req.method} ${req.originalUrl}`);
-  // eslint-disable-next-line no-console
-  console.error(`[STATUS] ${statusCode}`);
-  // eslint-disable-next-line no-console
-  console.error(`[MESSAGE] ${message}`);
+  if (process.env.NODE_ENV === 'development') {
+    console.error(`[ERROR] ${req.method} ${req.originalUrl}`);
+    console.error(`[STATUS] ${statusCode}`);
+    console.error(`[MESSAGE] ${message}`);
+    console.error(`[STACK] ${err.stack}`);
+  } else {
+    console.error(`[ERROR] ${message}`);
+  }
 
   res.status(statusCode).json({
     success: false,
