a121a0b5b1a3fd08541e66fcb25f9837efb8929f
JONGJIN-LEE1 <jin1122877@naver.com>
2025-07-25 15:37:40 +0900

    feat: customer.dto 추가

---
+++ src/customers/dto/customer.dto.ts
@@ -0,0 +1,90 @@
+/* eslint-disable max-classes-per-file */
+import { IsString, IsEnum, IsEmail, IsOptional, Length, IsInt, Min } from 'class-validator';
+import { Transform } from 'class-transformer';
+import { Gender, AgeGroup, Region } from '@prisma/client';
+
+export class CreateCustomerDto {
+  @IsString()
+  @Length(1, 50)
+  name!: string;
+
+  @IsOptional()
+  @IsEnum(Gender)
+  gender?: Gender;
+
+  @IsString()
+  @Length(1, 20)
+  phoneNumber!: string;
+
+  @IsOptional()
+  @IsEnum(AgeGroup)
+  ageGroup?: AgeGroup;
+
+  @IsOptional()
+  @IsEnum(Region)
+  region?: Region;
+
+  @IsOptional()
+  @IsEmail()
+  @Length(1, 100)
+  email?: string;
+
+  @IsOptional()
+  @IsString()
+  memo?: string;
+}
+
+export class UpdateCustomerDto {
+  @IsOptional()
+  @IsString()
+  @Length(1, 50)
+  name?: string;
+
+  @IsOptional()
+  @IsEnum(Gender)
+  gender?: Gender;
+
+  @IsOptional()
+  @IsString()
+  @Length(1, 20)
+  phoneNumber?: string;
+
+  @IsOptional()
+  @IsEnum(AgeGroup)
+  ageGroup?: AgeGroup;
+
+  @IsOptional()
+  @IsEnum(Region)
+  region?: Region;
+
+  @IsOptional()
+  @IsEmail()
+  @Length(1, 100)
+  email?: string;
+
+  @IsOptional()
+  @IsString()
+  memo?: string;
+}
+
+export class CustomerListQueryDto {
+  @IsOptional()
+  @Transform(({ value }) => parseInt(value, 10))
+  @IsInt()
+  @Min(1)
+  page?: number = 1;
+
+  @IsOptional()
+  @Transform(({ value }) => parseInt(value, 10))
+  @IsInt()
+  @Min(1)
+  limit?: number = 10;
+
+  @IsOptional()
+  @IsString()
+  search?: string;
+}
+
+export class UploadCustomersDto {
+  // 파일 업로드는 multer를 통해 처리되므로 DTO는 비어있습니다
+}
