f5c1e308f17d9e964319114a976144cd6152f1db
JONGJIN-LEE1 <jin1122877@naver.com>
2025-07-28 11:51:51 +0900

    refactor: app에서 customer 관련 라우터 분리

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,11 @@
 import express from 'express';
-import { PrismaClient } from '@prisma/client';
-import createCustomerRoutes from './customers/customers.routes';
 import { errorHandler } from './middlewares/error.middleware';
 import indexRouter from './index.routes';
 
 const app = express();
-const prisma = new PrismaClient();
 
 app.use(express.json());
 app.use('/', indexRouter);
-
-// Customer 라우트 등록
-app.use('/customers', createCustomerRoutes(prisma));
 
 // 에러 핸들링 미들웨어는 모든 라우트 뒤에 위치
 app.use(errorHandler);
