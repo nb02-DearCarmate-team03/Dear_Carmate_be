f96f325b1b0fdacb2e971af6635c0abf7ce03303
Kim_SeulBee <stella_62@naver.com>
2025-07-28 14:44:36 +0900

    feat: merge 및 lint, schema추적 제거 및  gitignore 충돌 병합 수정

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
 import express from 'express';
-import errorHandler from './middlewares/error.middleware'; // default import 사용
-import indexRouter from './index.routes'; // 라우터 추가
+import errorHandler from './middlewares/error.middleware';
+import indexRouter from './index.routes'; 
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/', indexRouter); // 라우터 연결
+app.use('/', indexRouter); 
 
 // 에러 핸들링 미들웨어는 모든 라우트 뒤에 위치
 app.use(errorHandler);
