a003daf36db8f5d68ee394990dcd50df2a9c4d76
sumin <suminon@naver.com>
2025-07-28 11:28:26 +0900

    chore: morgan,passport, cookieParser 등록

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,18 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+import passport from './auth/passport';
 
 import indexRouter from './index.routes';
 
 const app = express();
+
+app.use(morgan('dev'));
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(passport.initialize());
 
 app.use('/', indexRouter);
 
