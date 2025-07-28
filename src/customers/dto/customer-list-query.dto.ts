32be729ada735c44542fa934a274f9fe78f920dd
JONGJIN-LEE1 <jin1122877@naver.com>
2025-07-28 11:25:53 +0900

    dto 각 클래스별로 분리

---
+++ src/customers/dto/customer-list-query.dto.ts
@@ -0,0 +1,25 @@
+import { IsString, IsEnum, IsEmail, IsOptional, Length, IsInt, Min } from 'class-validator';
+import { Transform } from 'class-transformer';
+import { Gender, AgeGroup, Region } from '@prisma/client';
+
+export class CustomerListQueryDto {
+  @IsOptional()
+  @Transform(({ value }) => parseInt(value, 10))
+  @IsInt()
+  @Min(1)
+  page?: number = 1;
+
+  @IsOptional()
+  @Transform(({ value }) => parseInt(value, 10))
+  @IsInt()
+  @Min(1)
+  pageSize?: number = 10;
+
+  @IsOptional()
+  @IsString()
+  searchBy?: string;
+
+  @IsOptional()
+  @IsString()
+  keyword?: string;
+}
