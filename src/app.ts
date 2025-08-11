6cd005bd32a830f948c869a4fc5214baa1648a93
Kim_SeulBee <stella_62@naver.com>
2025-08-11 10:49:33 +0900

    fix: test 값 삭제

--- src/app.ts
+++ src/app.ts
@@ -8,9 +8,6 @@ import errorHandler from './middlewares/error.middleware';
 import passport from './auth/passport';
 
 import indexRouter from './index.routes';
-import contractRouter from './contracts/contracts.routes';
-import userRouter from './users/users.routes';
-import authRouter from './auth/auth.routes';
 
 const app = express();
 const prisma = new PrismaClient();
@@ -27,9 +24,7 @@ app.use(cookieParser());
 app.use(passport.initialize());
 
 app.use('/', indexRouter);
-app.use('/auth', authRouter(prisma));
 
-// 에러 핸들링 미들웨어는 모든 라우트 뒤에 위치
 app.use(errorHandler);
 
 export default app;
